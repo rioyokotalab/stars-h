639fbef3647b29919f70dd8fd187f4a7d1a6d87f
Aleksandr Mikhalev <aleksandr.mikhalev@kaust.edu.sa>
2018-12-23 17:03:16 +0300

    Fix bug with MPI in mpi_starpu_kblas

--- src/backends/mpi_starpu_kblas/blrm/drsdd.c
+++ src/backends/mpi_starpu_kblas/blrm/drsdd.c
@@ -579,7 +579,7 @@ int starsh_blrm__drsdd_mpi_starpu_kblas(STARSH_blrm **matrix,
         starsh_blrf_free(F2);
     }
     // Compute near-field blocks if needed
-    if(onfly == 0 && new_nblocks_near > 0)
+    if(onfly == 0 && new_nblocks_near_local > 0)
     {
         STARSH_MALLOC(near_D, new_nblocks_near_local);
         size_t size_D = new_nblocks_near_local * nb * nb;
