641e129bca142a193b6dc9811d15569d1e9ea269
Aleksandr Mikhalev <aleksandr.mikhalev@kaust.edu.sa>
2019-02-13 11:33:04 +0300

    Attempt to fix scalability of KBLAS on STARPU_KBLAS backend

--- include/starsh-starpu-kblas.h
+++ include/starsh-starpu-kblas.h
@@ -30,6 +30,8 @@
 //        int maxrank, double tol, int onfly);
 int starsh_blrm__drsdd_starpu_kblas(STARSH_blrm **matrix, STARSH_blrf *format,
         int maxrank, double tol, int onfly);
+int starsh_blrm__drsdd_starpu_kblas2(STARSH_blrm **matrix, STARSH_blrf *format,
+        int maxrank, double tol, int onfly);
 //int starsh_blrm__dqp3_starpu(STARSH_blrm **matrix, STARSH_blrf *format,
 //        int maxrank, double tol, int onfly);
 //int starsh_blrm__dna_starpu(STARSH_blrm **matrix, STARSH_blrf *format,
@@ -51,11 +53,11 @@ int starsh_blrm__drsdd_starpu_kblas(STARSH_blrm **matrix, STARSH_blrf *format,
 //void starsh_dense_dlrsdd_starpu(void *buffers[], void *cl_arg);
 void starsh_dense_dlrrsdd_starpu_kblas_cpu(void *buffers[], void *cl_arg);
 void starsh_dense_dlrrsdd_starpu_kblas_gpu(void *buffers[], void *cl_arg);
-void starsh_dense_dlrrsdd_starpu_kblas_cpu_S(void *buffers[], void *cl_arg);
-void starsh_dense_dlrrsdd_starpu_kblas_gpu_dgemm(void *buffers[],
-        void *cl_arg);
+void starsh_dense_dlrrsdd_starpu_kblas2_gpu(void *buffers[], void *cl_arg);
+void starsh_dense_dlrrsdd_starpu_kblas2_getrank(void *buffers[], void *cl_arg);
 //void starsh_dense_dlrqp3_starpu(void *buffers[], void *cl_arg);
 void starsh_dense_kernel_starpu_kblas_cpu(void *buffers[], void *cl_arg);
+void starsh_dense_kernel_starpu_kblas2_cpu(void *buffers[], void *cl_arg);
 //void starsh_dense_dgemm_starpu(void *buffers[], void *cl_arg);
 //void starsh_dense_fake_init_starpu(void *buffers[], void *cl_arg);
 
