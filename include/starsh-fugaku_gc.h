cc0767ccdb195cb9e985f924f0db81f8a92be974
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-03-24 19:49:26 -0400

    update starsh function

--- include/starsh-fugaku_gc.h
+++ include/starsh-fugaku_gc.h
@@ -19,4 +19,14 @@ typedef struct starsh_laplace
     //!< Value to add to each diagonal element (for positive definiteness).
 } STARSH_laplace;
 
+int starsh_normal_grid_generate(STARSH_particles** data, STARSH_int N,
+    STARSH_int ndim);
+
+void starsh_laplace_block_kernel(int nrows, int ncols, STARSH_int *irow,
+        STARSH_int *icol, void *row_data, void *col_data, void *result,
+        int ld);
+
+int starsh_laplace_grid_generate(STARSH_laplace **data, STARSH_int N,
+        STARSH_int ndim,
+        STARSH_int block_size, STARSH_int nblocks, double PV);
 #endif
